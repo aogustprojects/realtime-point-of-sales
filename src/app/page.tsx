d556a4b5d756e273e5cfc864864a473058b2367c
aogustprojects <aogust.ba@gmail.com>
2025-07-19 07:58:10 +0700

    19/7/2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { DarkModeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -5,7 +6,8 @@ export default function Home() {
   return (
     <div>
       <Input />
-      <Button>Hello</Button>
+      <Button className="dark:bg-amber-500 bg-blue-600">Hello</Button>
+      <DarkModeToggle />
     </div>
   );
 }
